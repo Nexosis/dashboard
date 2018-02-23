e7d675858ff0d1249a11df2bda3b743011d1c4dd
Jeff Gabriel <gabe19@yahoo.com>
2018-02-23 12:19:32 -0500

    session types and encoders

--- src/Page/Sessions.elm
+++ src/Page/Sessions.elm
@@ -1,8 +1,15 @@
 module Page.Sessions exposing (Model, Msg, init, update, view)
 
 import Data.Config exposing (Config)
+import Data.Session exposing (..)
+import Dict exposing (Dict)
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import RemoteData as Remote
+import Request.Session exposing (get)
+import Table
 import Util exposing ((=>))
+import View.Grid as Grid
 
 
 ---- MODEL ----
@@ -11,13 +18,25 @@ import Util exposing ((=>))
 type alias Model =
     { pageTitle : String
     , pageBody : String
+    , sessionList : Remote.WebData SessionList
+    , tableState : Table.State
+    , config : Config
     }
 
 
 init : Config -> ( Model, Cmd Msg )
 init config =
-    Model "Sessions" "This is the list of Sessions"
-        => Cmd.none
+    let
+        req =
+            Request.Session.get config 0
+
+        loadSessionList =
+            req
+                |> Remote.sendRequest
+                |> Cmd.map SessionListResponse
+    in
+    Model "Sessions" "This is the list of Sessions" Remote.Loading (Table.initialSort "name") config
+        => loadSessionList
 
 
 
@@ -25,14 +44,19 @@ init config =
 
 
 type Msg
-    = Todo
+    = SessionListResponse (Remote.WebData SessionList)
+    | SetTableState Table.State
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Todo ->
-            ( model, Cmd.none )
+        SessionListResponse resp ->
+            { model | sessionList = resp } => Cmd.none
+
+        SetTableState newState ->
+            { model | tableState = newState }
+                => Cmd.none
 
 
 
@@ -44,4 +68,33 @@ view model =
     div []
         [ h2 [] [ text model.pageTitle ]
         , div [] [ text model.pageBody ]
+        , Grid.view .items (config model.config.toolTips) model.tableState model.sessionList
         ]
+
+
+config : Dict String String -> Grid.Config SessionData Msg
+config toolTips =
+    Grid.config
+        { toId = \a -> a.sessionId
+        , toMsg = SetTableState
+        , columns =
+            [ nameColumn
+            ]
+        }
+
+
+nameColumn : Grid.Column SessionData Msg
+nameColumn =
+    Grid.veryCustomColumn
+        { name = "Name"
+        , viewData = sessionNameCell
+        , sorter = Table.increasingOrDecreasingBy (\a -> a.name)
+        , headAttributes = [ class "left per30" ]
+        , headHtml = []
+        }
+
+
+sessionNameCell : SessionData -> Table.HtmlDetails Msg
+sessionNameCell model =
+    Table.HtmlDetails [ class "left name" ]
+        [ a [] [ text model.name ] ]
---
+++ src/Request/Session.elm
@@ -0,0 +1,31 @@
+module Request.Session exposing (..)
+
+import Data.Config as Config exposing (Config, withAuthorization)
+import Data.Session exposing (..)
+import Http
+import HttpBuilder exposing (RequestBuilder, withExpect)
+
+
+get : Config -> Int -> Http.Request SessionList
+get { baseUrl, token } page =
+    let
+        expect =
+            decodeSessionList
+                |> Http.expectJson
+
+        params =
+            pageParams page Config.pageSize
+    in
+    (baseUrl ++ "/models")
+        |> HttpBuilder.get
+        |> HttpBuilder.withExpect expect
+        |> HttpBuilder.withQueryParams params
+        |> withAuthorization token
+        |> HttpBuilder.toRequest
+
+
+pageParams : Int -> Int -> List ( String, String )
+pageParams page pageSize =
+    [ ( "page", page |> toString )
+    , ( "pageSize", pageSize |> toString )
+    ]
