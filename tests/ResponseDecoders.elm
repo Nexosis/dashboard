6e8359ea239f271a83b9f6a663dccf7334729e5a
Zach Bussey <zbussey@nexosis.com>
2018-02-27 17:16:26 -0500

    Fix tests.

--- tests/ResponseDecoders.elm
+++ tests/ResponseDecoders.elm
@@ -1,6 +1,7 @@
 module ResponseDecoders exposing (..)
 
 import AppRoutes exposing (Route)
+import Data.Message as Message
 import Data.Response as Response exposing (decodeResponse)
 import Expect
 import Json.Decode
@@ -22,15 +23,15 @@ xhrResponseTests =
                     Ok result ->
                         Expect.equal
                             [ { message = "Nexosis could not execute any algorithms to model this dataset. The most common cause of this error is that the dataset does not contain enough rows prior to the forecast start date to adequately train and test a model. Please make sure that the dataset contains at least 150 records prior to the forecast start date and try the request again."
-                              , severity = Response.Informational
+                              , severity = Message.Informational
                               , routeToResource = Just (AppRoutes.SessionDetail "015ece43-87b5-4edb-b2b8-15ca957333fe")
                               }
                             , { message = "Nexosis could not execute any algorithms to model this dataset. The most common cause of this error is that the dataset does not contain enough rows prior to the forecast start date to adequately train and test a model. Please make sure that the dataset contains at least 150 records prior to the forecast start date and try the request again."
-                              , severity = Response.Error
+                              , severity = Message.Error
                               , routeToResource = Just (AppRoutes.SessionDetail "015ece43-87b5-4edb-b2b8-15ca957333fe")
                               }
                             , { message = "Usage Alert - You have 0 Predictions left for this cycle."
-                              , severity = Response.Error
+                              , severity = Message.Error
                               , routeToResource = Just (AppRoutes.SessionDetail "015ece43-87b5-4edb-b2b8-15ca957333fe")
                               }
                             ]
