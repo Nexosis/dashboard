f7261768d649a3622aec24785a3a1364c4f69d27
Craig Vermeer <craig@vermeers.net>
2018-02-27 11:28:40 -0500

    Target Column Name

--- src/Page/SessionDetail.elm
+++ src/Page/SessionDetail.elm
@@ -10,6 +10,9 @@ import Request.Model exposing (getOne)
 import Request.Session exposing (..)
 import Util exposing ((=>))
 import AppRoutes
+import List exposing (head, filter)
+import Data.Columns exposing (ColumnMetadata)
+import Data.Columns as Role exposing (Role)
 
 type alias Model =
     { sessionId : String
@@ -103,6 +106,144 @@ viewSessionHeader model =
                         ]
                     ]
                 ]
+            , div [class "row"]
+                [
+                    div [class "col-sm-4"]
+                        [
+                            loadingOr viewSessionDetail
+                        ]
+                    , div [class "col-sm-4"]
+                        [
+                            
+                        ]
+                    , div [class "col-sm-4"]
+                        [
+                            
+                        ]
+                ]
+        ]
+
+
+viewSessionDetail : SessionData -> Html Msg
+viewSessionDetail session = 
+    if session.status == Completed then
+        viewCompletedSession session
+    else
+        viewPendingSession session
+        
+
+
+viewPendingSession : SessionData -> Html Msg
+viewPendingSession session = 
+    h5 [ class "mb15" ]
+    [ text "Session Status" ]
+
+viewCompletedSession : SessionData -> Html Msg
+viewCompletedSession session = 
+    let
+        isTarget : ColumnMetadata -> Bool
+        isTarget column =
+            column.role == Role.Target
+
+        targetColumnFromColumns : SessionData -> String
+        targetColumnFromColumns session = 
+            filter isTarget session.columns
+                |> head
+                |> columnName
+        
+        targetColumn : SessionData -> String
+        targetColumn session = 
+            case session.targetColumn of
+                Just target -> 
+                    target
+                Nothing -> 
+                    targetColumnFromColumns session
+        
+        columnName : Maybe ColumnMetadata -> String
+        columnName col =
+            case col of
+                Nothing -> ""
+                Just c -> c.name
+            
+
+    in
+        div []
+        [ h5 [ class "mt15 mb15" ]
+            [ text "Details" ]
+        , p []
+            [ strong []
+                [ text "Model: " ]
+            , a [ href "#" ]
+                [ text "{ModelName}" ]
+            ]
+        , p []
+            [ strong []
+                [ text "Source: " ]
+            , a [ href "#" ]
+                [ text session.dataSourceName ]
+            ]
+        , p []
+            [ strong []
+                [ text "Target Column: " ]
+            , text (targetColumn session)
+            ]
+        , p []
+            [ strong []
+                [ text "Algorithm: " ]
+            , text "{AlgorithmName}"
+            ]
+        , p [ class "small" ]
+            [ strong []
+                [ text "Metrics" ]
+            ]
+        , ul [ class "small algorithm-metrics" ]
+            [ li []
+                [ strong []
+                    [ text "meanAbsoluteError:" ]
+                , br []
+                    []
+                , text "1715319610074.247"
+                ]
+            , li []
+                [ strong []
+                    [ text "meanAbsolutePercentError:" ]
+                , br []
+                    []
+                , text "150408208493.396"
+                ]
+            , li []
+                [ strong []
+                    [ text "rootMeanSquareError:" ]
+                , br []
+                    []
+                , text "10855736798156.244"
+                ]
+            ]
+        , p []
+            [ a [ class "btn btn-xs secondary", href "dashboard-session-champion.html" ]
+                [ text "View algorithm contestants" ]
+            ]
+        ]
+
+viewSessionName : SessionData -> Html Msg
+viewSessionName session = 
+    div [class "col-sm-9"]
+        [h2 [class "mt10"][text session.name]
+    ]
+    
+
+viewSessionId : SessionData -> Html Msg
+viewSessionId session = 
+    div [ class "col-sm-4" ]
+        [ p [ class "small" ]
+            [ strong []
+                [ text "Session ID:" ]
+            , text session.sessionId
+            , a []
+                [ i [ class "fa fa-copy color-mediumGray" ]
+                    []
+                ]
+            ]
         ]
 
 loadingOrView : Remote.WebData SessionData -> (SessionData -> Html Msg) -> Html Msg
@@ -116,27 +257,6 @@ loadingOrView request view =
         _ ->
             div [] []
 
-
-viewSessionName : SessionData -> Html Msg
-viewSessionName resp = 
-    div [class "col-sm-9"]
-        [h2 [class "mt10"][text resp.name]
-    ]
-    
-
-viewSessionId : SessionData -> Html Msg
-viewSessionId resp = 
-    div [ class "col-sm-4" ]
-        [ p [ class "small" ]
-            [ strong []
-                [ text "Session ID:" ]
-            , text resp.sessionId
-            , a []
-                [ i [ class "fa fa-copy color-mediumGray" ]
-                    []
-                ]
-            ]
-        ]
 
 init : Config -> String -> ( Model, Cmd Msg )
 init config sessionId =
