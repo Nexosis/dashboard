12ffe8c9085b90a1b01c2e1cdb2b071f6440cadb
Zach Bussey <zbussey@nexosis.com>
2018-03-02 09:55:47 -0500

    Disable click when loading.

--- src/Page/DataSetAdd.elm
+++ src/Page/DataSetAdd.elm
@@ -360,29 +360,32 @@ viewChooseUploadType model =
 viewSetKey : Model -> Html Msg
 viewSetKey model =
     let
-        ( createButtonContent, errorDisplay ) =
+        ( buttonAttributes, createButtonContent, errorDisplay ) =
             case model.activeTab of
                 FileUploadTab _ ->
                     case model.uploadResponse of
                         Remote.Loading ->
-                            ( spinner, viewRemoteError model.uploadResponse )
+                            ( [], spinner, viewRemoteError model.uploadResponse )
 
                         _ ->
-                            ( text "Create DataSet", viewRemoteError model.uploadResponse )
+                            ( [ onClick CreateDataSet ], text "Create DataSet", viewRemoteError model.uploadResponse )
 
                 _ ->
                     case model.importResponse of
                         Remote.Loading ->
-                            ( spinner, viewRemoteError model.importResponse )
+                            ( [], spinner, viewRemoteError model.importResponse )
 
                         Remote.Success importDetail ->
                             if importDetail.status == Status.Failed || importDetail.status == Status.Cancelled then
-                                ( i [ class "fa fa-upload mr5" ] [ text "Import" ], viewMessagesAsError importDetail.messages )
+                                ( [ onClick CreateDataSet ], i [ class "fa fa-upload mr5" ] [ text "Import" ], viewMessagesAsError importDetail.messages )
                             else
-                                ( spinner, viewMessagesAsError importDetail.messages )
+                                ( [], spinner, viewMessagesAsError importDetail.messages )
 
                         _ ->
-                            ( i [ class "fa fa-upload mr5" ] [ text "Import" ], viewRemoteError model.importResponse )
+                            ( [ onClick CreateDataSet ], i [ class "fa fa-upload mr5" ] [ text "Import" ], viewRemoteError model.importResponse )
+
+        addButton =
+            button (class "btn" :: buttonAttributes) [ createButtonContent ]
     in
     div [ class "col-sm-12" ]
         [ div [ id "review" ] <| viewEntryReview model
@@ -394,8 +397,7 @@ viewSetKey model =
                 , input [ class "form-control", placeholder "(Optional)", value model.key ] []
                 ]
             , div [ class "form-group" ]
-                [ button [ class "btn", onClick CreateDataSet ] [ createButtonContent ]
-                ]
+                [ addButton ]
             , errorDisplay
             ]
         , div [ class "col-sm-6" ]
