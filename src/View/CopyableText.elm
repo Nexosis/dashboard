fb9c34196f55f41ddf93a707e89e2bf535c40267
Craig Vermeer <craig@vermeers.net>
2018-03-15 11:55:01 -0400

    Little bit of visibility for when you click the 'Copy' button

--- src/View/CopyableText.elm
+++ src/View/CopyableText.elm
@@ -1,14 +1,18 @@
 module View.CopyableText exposing (copyableText)
 
 import Html exposing (Html, a, i, span, text)
-import Html.Attributes exposing (attribute, class)
+import Html.Attributes exposing (attribute, class, href)
 
 
 copyableText : String -> Html msg
 copyableText content =
     span [ class "small" ]
         [ text content
-        , a []
-            [ i [ class "fa fa-copy color-mediumgray ml5 copyToClipboard", attribute "data-clipboard-text" content ] []
+        , a
+            [ class "btn btn-sm p0 m0 copyToClipboard"
+            , attribute "role" "button"
+            , attribute "data-clipboard-text" content
+            ]
+            [ i [ class "fa fa-copy color-mediumgray ml5" ] []
             ]
         ]
--- src/index.js
+++ src/index.js
@@ -191,6 +191,16 @@ fetch('./config.json', { cache: 'no-store' }).then(function (response) {
             });
 
             const clipboard = new ClipboardJS('.copyToClipboard');
+            clipboard.on('success', function (e) {
+                
+                e.trigger.setAttribute("data-balloon", "Copied!");
+                e.trigger.setAttribute("data-balloon-pos", "right");
+
+                setTimeout(function() {
+                    e.trigger.removeAttribute("data-balloon");
+                    e.trigger.removeAttribute("data-balloon-pos")
+                }, 500)
+            });
         });
 
         
