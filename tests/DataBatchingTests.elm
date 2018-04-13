0c054e817368b599f84042ba0b61ef85787eb734
Craig Vermeer <craig@vermeers.net>
2018-04-13 14:39:45 -0400

    Fix tests

--- tests/DataBatchingTests.elm
+++ tests/DataBatchingTests.elm
@@ -54,16 +54,28 @@ batchingTests =
         ]
 
 
+asCsv content =
+    List.tail content
+        |> Maybe.withDefault []
+        |> CsvData (parseCsv content |> .headers)
+
+
+parseCsv csv =
+    List.take 100 csv
+        |> String.join "\x0D\n"
+        |> Csv.parse
+
+
 batchSizeTests : Test
 batchSizeTests =
     describe "batch size tests"
         [ test "calculates correct batch size for wide csv data"
             (\_ ->
-                Expect.lessThan 1500 <| calculateCsvBatchSize <| Csv.parse wideCsvData
+                Expect.lessThan 1500 <| calculateCsvBatchSize <| asCsv wideCsvData
             )
         , test "calculates correct batch size for narrow csv data"
             (\_ ->
-                Expect.greaterThan 30000 <| calculateCsvBatchSize <| Csv.parse narrowCsvData
+                Expect.greaterThan 30000 <| calculateCsvBatchSize <| asCsv narrowCsvData
             )
         , test "calculates correct batch size for narrow json data"
             (\_ ->
@@ -77,17 +89,15 @@ batchSizeTests =
 
 
 wideCsvData =
-    """
-    _unit_id,_golden,_unit_state,_trusted_judgments,_last_judgment_at,is_the_term,is_the_term:confidence,relevance,relevance:variance,definition,dist,example,freq,id,is_the_term_gold,len,relevance_gold,search,seed,term,topic
-867851139,false,finalized,3,1/28/2016 19:02:18,a_specific_person_or_place,0.5019,4.0,0.0,"Any humanitarian crisis or natural disaster situation involving water supply, including lack of water, contamination of water, etc.",0.699343922434,"* There are also fears of disease from ruined sewage systems and drinking water sources, but Health Minister Mohammad Naseer Khan told Reuters vaccination teams were fanning out to innoculate people against diseases such as cholera and tetanus.",11.0,674,,3,,https://www.google.com/search?q=%22FROM%20RUINED%20SEWAGE%22,0,FROM RUINED SEWAGE,Water
-    """
+    [ "_unit_id,_golden,_unit_state,_trusted_judgments,_last_judgment_at,is_the_term,is_the_term:confidence,relevance,relevance:variance,definition,dist,example,freq,id,is_the_term_gold,len,relevance_gold,search,seed,term,topic"
+    , "867851139,false,finalized,3,1/28/2016 19:02:18,a_specific_person_or_place,0.5019,4.0,0.0,\"Any humanitarian crisis or natural disaster situation involving water supply, including lack of water, contamination of water, etc.\", 0.699343922434, \"* There are also fears of disease from ruined sewage systems and drinking water sources, but Health Minister Mohammad Naseer Khan told Reuters vaccination teams were fanning out to innoculate people against diseases such as cholera and tetanus.\",11.0,674,,3,,https://www.google.com/search?q=%22FROM%20RUINED%20SEWAGE%22,0,FROM RUINED SEWAGE,Water"
+    ]
 
 
 narrowCsvData =
-    """
-    iris,sepal_len,sepal_width,petal_len,petal_width
-setosa,5.1,3.5,1.4,0.2
-    """
+    [ "iris,sepal_len,sepal_width,petal_len,petal_width"
+    , "setosa,5.1,3.5,1.4,0.2"
+    ]
 
 
 narrowJsonData =
